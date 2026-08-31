e30c769649c70c6ceba2fe384283ff5c46b8d950
Shaun Ramsey <shaun.ramsey@gmail.com>
2026-08-31 16:39:08 -0400

    2d to 1d conversion in comments

--- first.dart
+++ first.dart
@@ -1,5 +1,16 @@
 import 'dart:io';
 
+// 2d to 1d conversions
+// in two dimension you have "x" position i, and "y" position called j
+// lookup the value of 2nd row (y), 3rd number (x)
+// this file has width *5*, 2nd row is j=1. 3rd number is i=2
+// int oned(int i, int j, int w) => j * w + i;
+// 0 x x x 4
+// 5 6 7 x x
+// x x x x x
+// 15 x x x x
+// 20 x x x 24
+
 void main() {
   File file = File('test.txt');
   String contents = file.readAsStringSync();

e8eb5fa0277ba65a2b025b0d6b32684d28599602
Shaun Ramsey <shaun.ramsey@gmail.com>
2026-09-02 16:46:32 -0400

    another test file, updates to add class Grid

--- first.dart
+++ first.dart
@@ -1,50 +1,51 @@
 import 'dart:io';
 
-// 2d to 1d conversions
-// in two dimension you have "x" position i, and "y" position called j
-// lookup the value of 2nd row (y), 3rd number (x)
-// this file has width *5*, 2nd row is j=1. 3rd number is i=2
-// int oned(int i, int j, int w) => j * w + i;
-// 0 x x x 4
-// 5 6 7 x x
-// x x x x x
-// 15 x x x x
-// 20 x x x 24
-
-// [ [x, x, x, x, x],
-//   [x, x, x, x, x],
-//   [x, x, x, x, x],
-//   [x, x, x, x, x],
-//   [x, x, x, x, x]
-// ]
-
-
-void main() {
-  File file = File('test.txt');
-  String contents = file.readAsStringSync();
-  List<String> lines = contents.split('\n');
-  print('---------------------');
-  for (String line in lines) {
-    List<String> parts = line.split(' ');
-    for (String part in parts) {
-      int number = int.parse(part);
-      print("number is $number");
+class Grid {
+  List<List<int>> grid = [];
+  
+  @override
+  String toString() {
+    String returnString = "";
+    for (List<int> list in grid) {
+      returnString += "$list\n";
     }
-    print("--- new line---");
+    return returnString;
   }
 
+  int countThree() {
+    int sum = 0;
+    for( List<int> lines in grid ) {
+      for( int number in lines ) {
+        if(number == 3) {
+          sum++;
+        }
+      }
+    }
+    return sum;
+  }
 
-  List< List<int> > matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]];
-  int x = 0;
-  print("${matrix[x-1][2]}");
-
+  void readFromFile(String filename) {
+    File file = File(filename);
+    String contents = file.readAsStringSync();
+    List<String> lines = contents.split('\n');
+    // print('---------------------');
+    for (String line in lines) {
+      List<int> thisLine = [];
+      List<String> parts = line.split(' ');
+      for (String part in parts) {
+        int number = int.parse(part);
+        // print("number is $number");
+        thisLine.add(number);
+      }
+      grid.add(thisLine);
+    }
+    grid.removeAt(0);
+  }
 }
 
-
-// read in a file
-// 3
-// 1 2 3
-// 3 3 3
-// 1 1 1
-
-// How many 3's are in it.
+void main() {
+  Grid grid = Grid();
+  grid.readFromFile("test2.txt");
+  print("I'm here\n$grid");
+  print("Count 3 is ${grid.countThree()}");
+}
